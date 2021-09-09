1c2e9fa2906ab11b8e591356fce1ef754a19bf06
rohman <rohmann311@gmail.com>
2021-09-09 11:02:03 +0700

    add device information in dialog message

--- lup/src/main/java/com/icodeu/lup/exception/ShowDefaultDialog.kt
+++ lup/src/main/java/com/icodeu/lup/exception/ShowDefaultDialog.kt
@@ -3,12 +3,16 @@ package com.icodeu.lup.exception
 import android.content.Context
 import com.icodeu.lup.repo.ErrorData
 import com.icodeu.lup.ui.LupMainActivity
+import com.icodeu.lup.utils.getInfosAboutDevice
 import com.icodeu.lup.utils.intentTo
 
 class ShowDefaultDialog(val context: Context, val message: String, val reportUrl: String?) :
     ExceptionHandler {
 
     override fun doOnExceptionListener(thread: Thread, throwable: Throwable) {
-        context.intentTo(LupMainActivity::class.java, ErrorData(message,throwable.stackTraceToString()), reportUrl)
+        val stackTrace = "${throwable.stackTraceToString()} \n" +
+                "-- Device Information --" +
+                context.getInfosAboutDevice()
+        context.intentTo(LupMainActivity::class.java, ErrorData(message,stackTrace), reportUrl)
     }
 }
